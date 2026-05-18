35f7b96082fe2e68ef16dffeedb4496be246c612
João Lucas Pinheiro <lucasdallas25@gmail.com>
2026-05-17 22:52:11 -0300

    feat: enhance login page with email and password fields, show/hide password functionality, and improved styling

--- virta-front-end/src/pages/login.tsx
+++ virta-front-end/src/pages/login.tsx
@@ -1,8 +1,13 @@
 import { Waves } from "lucide-react";
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
 
 export function LoginPage() {
   const { t, i18n } = useTranslation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
@@ -47,7 +52,51 @@ export function LoginPage() {
 
       {/* Formulario */}
       <div className="flex-1 bg-zinc-50 flex items-center justify-center">
-        <h1 className="text-zinc-900"> Área de login</h1>
+        <form className="w-full max-w-md bg-zinc-50 p-8 rounded-2xl shadow-lg">
+          <h2 className="text-2xl font-bold text-zinc-900 mb-2 text-center">
+            Bem-vindo ao Virta
+          </h2>
+          <p className="text-sm text-zinc-600 text-center mb-6 leading-tight">
+            Faça login ou registre-se
+          </p>
+          <div className="mb-4">
+            <label className="block text-sm font-medium mb-2">Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-4 py-3 border  rounded-lg focus:outline-none focus:ring-1 focus:ring-brand-500"
+              placeholder="Digite seu email"
+            />
+          </div>
+
+          <div className="mb-6">
+            <label className="block text-sm font-medium mb-2">Senha</label>
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full px-4 py-3 border  rounded-lg focus:outline-none focus:ring-1 focus:ring-brand-500"
+                placeholder="Digite sua senha"
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword((prev) => !prev)}
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-zinc-500 hover:text-zinc-800"
+              >
+                {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+              </button>
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-brand-500 text-white py-3 rounded-lg"
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     </div>
   );
